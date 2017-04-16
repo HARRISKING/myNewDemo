1407e8de8de40209848879619cbcfed94ef5bbb2
unknown <>
2017-04-16 15:21:35 +0800

    build

--- webpack-react3/myreact-test1/src/index.js
+++ webpack-react3/myreact-test1/src/index.js
@@ -7,3 +7,10 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+ReactDOM.render(
+  <div>
+    <h1>这是我的第n个react代码，妈的我都不知道注册了这么多干毛用！！！</h1>
+  </div>,
+  document.getElementById('root')
+)
