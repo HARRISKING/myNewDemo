395c26483bc9461701c4435fc3caed584d7f64b3
HARRISKING <llc13588440410@outlook.com>
2017-06-12 21:27:08 +0800

    6.12

---
+++ myReact4/react1/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+ReactDOM.render(
+  <h1>fuck yourself,hahah!</h1>,
+  document.getElementById('root2')
+)
