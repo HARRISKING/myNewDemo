301a92c175974913e098c5c4e93732937c486ae6
HARRISKING <llc13588440410@outlook.com>
2017-06-12 13:13:00 +0800

    6.12

--- myReact4/webpack-demo/app/index.js
+++ myReact4/webpack-demo/app/index.js
@@ -1,15 +1,17 @@
-import _ from 'lodash';
-import $ from 'jquery';
-function component () {
-  // var element = document.createElement('div');
-  var element = $('<div></div>')
+// import _ from 'lodash';
+// import $ from 'jquery';
+// function component () {
+//   // var element = document.createElement('div');
+//   var element = $('<div></div>')
+//
+//   /* lodash is required for the next line to work */
+//   // element.innerHTML = _.join(['Hello','webpack'], ' ');
+//   element.html(_.join(['hello','webpack','asdfas'],' '))
+//
+//   // return element;
+//   return element.get(0);
+// }
+//
+// document.body.appendChild(component());
 
-  /* lodash is required for the next line to work */
-  // element.innerHTML = _.join(['Hello','webpack'], ' ');
-  element.html(_.join(['hello','webpack','asdfas'],' '))
-
-  // return element;
-  return element.get(0);
-}
-
-document.body.appendChild(component());
+console.log(1);
